61762c30c7ed13bfd50bda880bf61f97bac8be28
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-08-21 23:03:09 +0100

    updating translation files

--- epsihome_site/src/component/Service.jsx
+++ epsihome_site/src/component/Service.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ServiceCard from "./common/ServiceCard";
 import data from "../constant/data";
+import { useTranslation } from "react-i18next";
 
 function Service() {
+  const {t} = useTranslation()
   return (
     <section id="services" className="bg-white py-12 mt-4">
-      <h1 className="text-3xl text-center my-4 text-gray-800 uppercase font-bold">our services</h1>
+      <h1 className="text-3xl text-center my-4 text-gray-800 uppercase font-bold">{t("services.title")}</h1>
       <div className="container mx-auto px-4">
         <h2 className="text-4xl font-bold mb-8">Our Services</h2>
         <div className="flex flex-wrap -mx-4">
