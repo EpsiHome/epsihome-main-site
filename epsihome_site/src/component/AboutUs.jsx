17e002d32e70c20003c55285d9abfc24782f389d
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-08-27 22:52:17 +0100

    feat:(translating the about us section)

--- epsihome_site/src/component/AboutUs.jsx
+++ epsihome_site/src/component/AboutUs.jsx
@@ -1,53 +1,28 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const AboutUs = () => {
+  const { t } = useTranslation();
+
   return (
-    <section id="about" className="about-us h-screen bg-white justify-center">
-      <h1 className="text-3xl text-center my-4 text-gray-800 uppercase font-bold">about us</h1>
-      <div className="grid grid-cols-2 gap-4">
+    <section id="about" className="about-us h-screen overflow-scroll bg-white justify-center">
+      <h1 className="text-3xl text-center my-4 text-gray-800 uppercase font-bold">
+        {t("aboutUs.title")}
+      </h1>
+      <div className="grid grid-cols-2 px-4 gap-4">
         <div>
-          <p className="text-gray-700 mb-4">
-            We are a proptech company that provides a suite of services to help
-            people buy, sell, and rent homes. Our mission is to make the real estate
-            process more efficient and transparent for everyone involved.
-          </p>
-          <p className="text-gray-700 mb-4">
-            We believe that everyone should have access to affordable housing, and
-            we are committed to using our technology to make that a reality. We are
-            also passionate about creating a more sustainable real estate industry,
-            and we are working to implement innovative solutions to reduce our
-            environmental impact.
-          </p>
+          <p className="text-gray-700 mb-4">{t("aboutUs.description1")}</p>
+          <p className="text-gray-700 mb-4">{t("aboutUs.description2")}</p>
         </div>
         <div>
           <ul className="list-disc mb-4">
-            <li className="text-gray-700">
-              We provide a platform for buyers and sellers to connect with each
-              other.
-            </li>
-            <li className="text-gray-700">
-              We offer a variety of tools to help people find the right home for
-              their needs.
-            </li>
-            <li className="text-gray-700">
-              We provide transparent pricing and terms so that people can make
-              informed decisions.
-            </li>
-            <li className="text-gray-700">
-              We are committed to customer service and satisfaction.
-            </li>
-            <li className="text-gray-700">
-              We are committed to helping develop our economy by providing
-              employment and revolutionizing the way real estate related activities
-              are carried out in Cameroon and Africa.
-            </li>
-            <li className="text-gray-700">
-              We offer real estate investment tools to help people grow their
-              wealth.
-            </li>
-            <li className="text-gray-700">
-              We provide mortgages to help people own their own homes.
-            </li>
+            <li className="text-gray-700">{t("aboutUs.listItem1")}</li>
+            <li className="text-gray-700">{t("aboutUs.listItem2")}</li>
+            <li className="text-gray-700">{t("aboutUs.listItem3")}</li>
+            <li className="text-gray-700">{t("aboutUs.listItem4")}</li>
+            <li className="text-gray-700">{t("aboutUs.listItem5")}</li>
+            <li className="text-gray-700">{t("aboutUs.listItem6")}</li>
+            <li className="text-gray-700">{t("aboutUs.listItem7")}</li>
           </ul>
         </div>
       </div>
