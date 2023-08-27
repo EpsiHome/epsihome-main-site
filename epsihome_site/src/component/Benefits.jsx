fd7815de128d7fa771ed235d1b0a313fa7745be3
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-08-27 23:30:16 +0100

    fix:(vertical padding)

--- epsihome_site/src/component/Benefits.jsx
+++ epsihome_site/src/component/Benefits.jsx
@@ -48,7 +48,7 @@ const Benefits = () => {
   ];
 
   return (
-    <section id='benefits' className="flex justify-center items-center min-h-screen bg-gray-100">
+    <section id='benefits' className="flex py-4 justify-center items-center min-h-screen bg-gray-100">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {benefits.map((benefit, index) => (
           <BenefitsCard key={index} title={benefit.title} subtitle={benefit.subtitle} />
